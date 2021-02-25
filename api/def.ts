43905746457defda871bb12c9c356673803c5734
Austin Gatlin <austin@gatlin.io>
2021-02-25 17:28:26 +1100

    Fix bug and add origin whitelist.

--- api/def.ts
+++ api/def.ts
@@ -2,12 +2,20 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import { getFullDef } from '../src/api'
 import { FullDef, JSONPayload } from '../src/types'
 
+const isBadOrigin = (req: NowRequest): boolean => {
+  const originWhitelist = ['http://localhost:3000', 'https://www.gatlin.io']
+  return !originWhitelist.includes(req.headers.origin)
+}
+
 export default async function (req: NowRequest, res: NowResponse) {
+  if (isBadOrigin(req)) {
+    res.status(400)
+    return
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
   const { word } = req.query
   const fullDef: FullDef = await getFullDef(word as string)
   const payload: JSONPayload<FullDef> = { data: fullDef }
-  res
-  .status(200)
-  .json(payload)
-  .setHeader('Access-Control-Allow-Origin', 'http://localhost:3000,https://www.gatlin.io')
+  res.status(200).json(payload)
 }
