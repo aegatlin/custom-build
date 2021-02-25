700a7c150270417f1f08835df9ffbe5c842d31e8
Austin Gatlin <austin@gatlin.io>
2021-02-25 11:48:57 +1100

    Try another import strat for wordpos package.

--- api/def.ts
+++ api/def.ts
@@ -4,7 +4,7 @@ import { FullDef, JSONPayload } from '../src/types'
 
 export default async function (req: NowRequest, res: NowResponse) {
   const { word } = req.query
-  const fullDef: FullDef = await getFullDef(word)
+  const fullDef: FullDef = await getFullDef(word as string)
   const payload: JSONPayload<FullDef> = { data: fullDef }
   res.status(200).json(payload)
 }
--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,9 @@
-import * as WordPOS from 'wordpos'
+import WordPOS from 'wordpos'
 import { FullDef } from './types'
 
 const wordpos = new WordPOS()
 
-export async function getFullDef(word): Promise<FullDef> {
+export async function getFullDef(word: string): Promise<FullDef> {
   const defs = await wordpos.lookup(word).map(r => r.def)
   return { word, defs }
 }
